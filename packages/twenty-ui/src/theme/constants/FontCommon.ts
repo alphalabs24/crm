7429355aa9e2ce68d396e3c647acdbc9e9ae350c
Lucas Pelloni <lucas_pelloni@hotmail.com>
2025-03-10 17:54:26 +0100

    satoshi

--- packages/twenty-ui/src/theme/constants/FontCommon.ts
+++ packages/twenty-ui/src/theme/constants/FontCommon.ts
@@ -15,3 +15,14 @@ export const FONT_COMMON = {
   },
   family: 'Geist, sans-serif',
 };
+
+export const FONT_FAMILY = {
+  primary: "'Satoshi', sans-serif",
+};
+
+export const FONT_WEIGHT = {
+  regular: '400',
+  medium: '500',
+  semiBold: '600',
+  bold: '700',
+};
