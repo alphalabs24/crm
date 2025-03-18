b5bc0bdacd9752d1071031d241a5ed24525a7c21
rnichi1 <71671446+rnichi1@users.noreply.github.com>
2025-03-18 13:57:18 +0100

    some better descriptions

--- packages/twenty-front/src/modules/onboarding-tutorial/constants/onboarding-steps.tsx
+++ packages/twenty-front/src/modules/onboarding-tutorial/constants/onboarding-steps.tsx
@@ -15,6 +15,7 @@ export type TutorialOnboardingStepData = {
   title: string | ReactNode;
   description: string | ReactNode;
   Icon: (props: TablerIconsProps) => ReactElement;
+  hidden?: boolean;
 };
 
 // These are the visible steps in the onboarding tutorial component
@@ -22,25 +23,38 @@ export const TUTORIAL_ONBOARDING_STEPS: TutorialOnboardingStepData[] = [
   {
     id: UserTutorialTask.TUTORIAL_PLATFORM_SETUP,
     title: <Trans>Setup your platform credentials</Trans>,
-    description: <Trans>Setup your platform credentials</Trans>,
+    description: (
+      <Trans>
+        Setup your platform credentials in order to publish your properties.
+      </Trans>
+    ),
     Icon: IconAbc,
   },
   {
     id: UserTutorialTask.TUTORIAL_EMAIL,
     title: <Trans>Connect your email</Trans>,
-    description: <Trans>Connect your email</Trans>,
+    description: (
+      <Trans>
+        Connect your email in order to synchronize your inbox with nestermind.
+      </Trans>
+    ),
     Icon: IconMail,
   },
   {
     id: UserTutorialTask.TUTORIAL_PROPERTY,
     title: <Trans>Create your first property</Trans>,
-    description: <Trans>Create your first property</Trans>,
+    description: (
+      <Trans>
+        A property in nestermind holds the master data about a property you want
+        to publish.
+      </Trans>
+    ),
     Icon: IconHome,
   },
   {
     id: UserTutorialTask.TUTORIAL_PUBLICATION,
     title: <Trans>Create your first publication</Trans>,
-    description: <Trans>Create your first publication</Trans>,
+    description: <Trans>Publish your first property to a platform.</Trans>,
     Icon: IconHomeShare,
   },
   {
@@ -48,5 +62,6 @@ export const TUTORIAL_ONBOARDING_STEPS: TutorialOnboardingStepData[] = [
     title: <Trans>Completed all steps</Trans>,
     description: <Trans>Completed all steps</Trans>,
     Icon: IconCheck,
+    hidden: true,
   },
 ];
