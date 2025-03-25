238063c01741f5f433b58aceff869cdb90b538dd
rnichi1 <71671446+rnichi1@users.noreply.github.com>
2025-03-25 14:25:05 +0100

    Update usePageChangeEffectNavigateLocation.ts

--- packages/twenty-front/src/hooks/usePageChangeEffectNavigateLocation.ts
+++ packages/twenty-front/src/hooks/usePageChangeEffectNavigateLocation.ts
@@ -99,7 +99,6 @@ export const usePageChangeEffectNavigateLocation = () => {
     isMatchingOnboardingRoute &&
     isLoggedIn
   ) {
-    return defaultHomePagePath;
     // after onboarding is completed, we want to redirect to the tutorial
     return AppPath.Tutorial;
   }
