d58ae7d0d8895e7c5d54e1249f1a2f981955dad0
severinwullschleger <severin.wullschleger@bluewin.ch>
2025-05-02 08:45:20 +0200

    move canny script to index.html

--- packages/twenty-front/src/index.tsx
+++ packages/twenty-front/src/index.tsx
@@ -6,12 +6,6 @@ import { App } from '@/app/components/App';
 import 'react-loading-skeleton/dist/skeleton.css';
 import './index.css';
 
-// Add Canny SDK script
-const script = document.createElement('script');
-script.setAttribute('nonce', 'NONCE_PLACEHOLDER');
-script.innerHTML = `!function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");`;
-document.head.appendChild(script);
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') ?? document.body,
 );
